5cddaa3a5520c19b3985d57ee84b27f1d0091102
jessica <s_jessica.menjivaraquino@ousd.org>
2018-11-01 04:56:07 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
+
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
+var userchoice ;
+var computerchoice;
+var winner;
 
 // DOCUMENT READY FUNCTION BELOW
 
+
+
+$("button").click(function() {
+    userchoice = $("input").val();
+    $("#userChoice").text(userchoice);
+    var randonNumber = Math.random();
+    if(randonNumber > .666){
+        computerchoice = 'rock';
+    }
+    else if (randomNumber < .666 && randonNumber > .333){
+        computerchoice = 'paper';
+    }
+     else{
+         computerchoice = 'scissors';
+     }   
+    }
+    
+});
